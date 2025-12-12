d43fba4207536066dbe9473ffd0298da33f72db9
Kristian <kristianhnielsen@protonmail.com>
2025-12-12 11:14:34 +0100

    refactor load_data function signature for type hinting

--- data/data.py
+++ data/data.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 
-def load_data(file_path="data/WA_Fn-UseC_-Telco-Customer-Churn.csv"):
+def load_data(
+    file_path: str = "data/WA_Fn-UseC_-Telco-Customer-Churn.csv",
+) -> pd.DataFrame:
     data = pd.read_csv(file_path)
     return data
