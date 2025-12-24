be50c6acde7c044f28f46bc2a65278b730bd24fb
Kristian <kristianhnielsen@protonmail.com>
2025-12-24 11:35:10 +0100

    make pir charts, log reg baseline, smote log reg

--- kris/eda.py
+++ kris/eda.py
@@ -22,15 +22,53 @@ def _(mo):
 
 @app.cell
 def _():
+    import math
     import pandas as pd
     import numpy as np
     import matplotlib.pyplot as plt
     import seaborn as sns
     import marimo as mo
     import altair as alt
-    from sklearn.preprocessing import StandardScaler, OneHotEncoder
-    from sklearn.decomposition import PCA
-    return OneHotEncoder, PCA, StandardScaler, alt, mo, np, pd, plt, sns
+    from sklearn.preprocessing import (
+        StandardScaler,
+        OneHotEncoder,
+        LabelEncoder,
+        MinMaxScaler,
+    )
+    from sklearn.decomposition import PCA, KernelPCA
+    from sklearn.model_selection import (
+        train_test_split,
+        RandomizedSearchCV,
+        GridSearchCV,
+        cross_val_score,
+    )
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.metrics import (
+        classification_report,
+        confusion_matrix,
+        roc_curve,
+        auc,
+        roc_auc_score,
+        accuracy_score,
+        RocCurveDisplay,
+    )
+    return (
+        LabelEncoder,
+        LogisticRegression,
+        OneHotEncoder,
+        PCA,
+        StandardScaler,
+        accuracy_score,
+        alt,
+        classification_report,
+        math,
+        mo,
+        np,
+        pd,
+        plt,
+        sns,
+        train_test_split,
+    )
 
 
 @app.cell(hide_code=True)
@@ -112,7 +150,94 @@ def _():
 @app.cell(column=1, hide_code=True)
 def _(mo):
     mo.md(r"""
-    # Correlation Heatmap
+    # General Plots
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Pie Charts
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    column_categories = {
+        "customer": ["seniorcitizen", "partner", "dependents"],
+        "account": ["contract", "paperlessbilling", "paymentmethod"],
+        "services": [
+            "onlinesecurity",
+            "onlinebackup",
+            "deviceprotection",
+            "techsupport",
+            "streamingtv",
+            "streamingmovies",
+            "phoneservice",
+            "multiplelines",
+            "internetservice",
+        ],
+        "churn": ["churn"],
+    }
+    return (column_categories,)
+
+
+@app.cell(hide_code=True)
+def _(column_categories, mo):
+    pie_select_col = mo.ui.radio(
+        options=[col for col in column_categories.keys()],
+        label="Select data category:",
+        value=list(column_categories.keys())[0],
+    )
+    return (pie_select_col,)
+
+
+@app.cell(hide_code=True)
+def _(column_categories, data, math, mo, pie_select_col, plt):
+    cols_in_selected_category = column_categories[pie_select_col.value]
+    num_plots = len(cols_in_selected_category)
+    cols_per_row = 3 if num_plots >= 3 else num_plots
+    num_rows = math.ceil(num_plots / cols_per_row)
+
+    _fig, _axes = plt.subplots(
+        num_rows, cols_per_row, figsize=(5 * cols_per_row, 5 * num_rows)
+    )
+
+    # Flatten axes array to 1D for easy looping (works even if rows=1)
+    # Note: If num_plots is 1, wrap axes in a list or handle separately
+    _axes = _axes.flatten() if num_plots > 1 else [_axes]
+
+
+    for _i, _col in enumerate(cols_in_selected_category):
+        data[_col].value_counts().plot.pie(
+            ax=_axes[_i],
+            autopct="%1.1f%%",
+            startangle=90,
+            counterclock=False,
+            title=f"Distribution of {_col}",
+        )
+        _axes[_i].set_ylabel("")
+        _axes[_i].set_xlabel("")
+
+    plt.tight_layout()
+    mo.vstack([pie_select_col, _fig])
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    Only a 26.6% of the observations are categorized as churn. Is this too imbalanced?
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Correlation Heatmap
     """)
     return
 
@@ -321,7 +446,6 @@ def _(
         index=data.index,
     )
 
-
     encoded_data = pd.concat(
         [
             data[numerical_cols],
@@ -381,6 +505,7 @@ def _(PCA, StandardScaler, alt, encoded_data, mo, np, pca_select_col, pd):
         }
     )
 
+
     # 3. Create Plots
 
     # Scree Plot (Bar Chart)
@@ -418,18 +543,240 @@ def _(PCA, StandardScaler, alt, encoded_data, mo, np, pca_select_col, pd):
         .mark_rule(color="red", strokeDash=[5, 5])
         .encode(y="y")
     )
+
+    # put dataframe back together with the pca
+    _df_full = encoded_data.copy()
+    _df_full.drop(columns=_selected_cols, inplace=True)
+    df_pca_combined = pd.concat(
+        [_df_full, pd.DataFrame(pca.transform(X_scaled))], axis=1
+    )
+
+
     mo.vstack(
         [
-            mo.md("**No columns selected for PCA. Please select at least one column to display PCA plots.**") if pca_select_col.value == [] else "",
+            mo.md(
+                "**No columns selected for PCA. Please select at least one column to display PCA plots.**"
+            )
+            if pca_select_col.value == []
+            else "",
             pca_select_col,
-            scree.interactive(), 
+            scree.interactive(),
             (cumulative + rule).interactive(),
         ]
     )
     return
 
 
-@app.cell(column=5)
+@app.cell(column=5, hide_code=True)
+def _(mo):
+    mo.md(r"""
+    # Logistic Regression Baseline
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## One Hot Encoded Data
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(OneHotEncoder, StandardScaler, data, numerical_cols, pd):
+    one_hot_encoder = OneHotEncoder(sparse_output=False)
+
+    # Identify column types
+    _binary_cols = [
+        col
+        for col in data.select_dtypes(exclude="float64").columns
+        if data[col].nunique() == 2 and col != "seniorcitizen"
+    ]
+    _ternary_cols = [
+        col
+        for col in data.select_dtypes(exclude="float64").columns
+        if data[col].nunique() == 3
+    ]
+    _categorical_cols = [
+        col
+        for col in data.select_dtypes(exclude="float64").columns
+        if data[col].nunique() > 3
+    ]
+
+    _numerical_cols = data.select_dtypes(include="float64").columns.tolist()
+
+
+    # Encode data of different types
+    _encoded_binary = data[_binary_cols].apply(
+        lambda x: x.map({"Yes": 1, "No": 0})
+    )
+
+    _encoded_ternary = pd.DataFrame(
+        one_hot_encoder.fit_transform(data[_ternary_cols]),
+        columns=one_hot_encoder.get_feature_names_out(_ternary_cols),
+        index=data.index,
+    )
+
+
+    _encoded_categorical = pd.DataFrame(
+        one_hot_encoder.fit_transform(data[_categorical_cols]),
+        columns=one_hot_encoder.get_feature_names_out(_categorical_cols),
+        index=data.index,
+    )
+
+
+    # Combine all encoded data
+    ohe_encoded_data = pd.concat(
+        [
+            data[_numerical_cols],
+            data[["seniorcitizen"]],
+            _encoded_categorical,
+            _encoded_binary,
+            _encoded_ternary,
+        ],
+        axis=1,
+    )
+
+    ohe_encoded_data[numerical_cols] = StandardScaler().fit_transform(
+        ohe_encoded_data[numerical_cols]
+    )
+    return (ohe_encoded_data,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ### Logistic Regression
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(
+    LogisticRegression,
+    accuracy_score,
+    classification_report,
+    ohe_encoded_data,
+    train_test_split,
+):
+    _y = ohe_encoded_data["churn"]
+    _X = ohe_encoded_data.drop(columns=["churn"])
+    _X_train, _X_test, _y_train, _y_test = train_test_split(
+        _X, _y, test_size=0.2, random_state=42, stratify=_y
+    )
+
+    ohe_log_reg = LogisticRegression(max_iter=10_000).fit(_X_train, _y_train)
+    ohe_preds = ohe_log_reg.predict(_X_test)
+    ohe_accuracy = accuracy_score(_y_test, ohe_preds)
+    print(f"Accuracy: {ohe_accuracy}")
+    print(classification_report(_y_test, ohe_preds))
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Label Encoded Data
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(LabelEncoder, StandardScaler, data, numerical_cols):
+    label_encoder = LabelEncoder()
+    label_encoded_data = data.copy()
+    for col in label_encoded_data.select_dtypes(
+        include=["object"]
+    ).columns.to_list():
+        label_encoded_data[col] = label_encoder.fit_transform(
+            label_encoded_data[col]
+        )
+    label_encoded_data[numerical_cols] = StandardScaler().fit_transform(
+        label_encoded_data[numerical_cols]
+    )
+    return (label_encoded_data,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ### Logistic Regression
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(
+    LogisticRegression,
+    accuracy_score,
+    classification_report,
+    label_encoded_data,
+    train_test_split,
+):
+    _y = label_encoded_data["churn"]
+    _X = label_encoded_data.drop(columns=["churn"])
+    _X_train, _X_test, _y_train, _y_test = train_test_split(
+        _X, _y, test_size=0.2, random_state=42, stratify=_y
+    )
+
+    le_log_reg = LogisticRegression(max_iter=10_000).fit(_X_train, _y_train)
+    le_preds = le_log_reg.predict(_X_test)
+    le_accuracy = accuracy_score(_y_test, le_preds)
+    print(f"Accuracy: {le_accuracy}")
+    print(classification_report(_y_test, le_preds))
+    return
+
+
+@app.cell(column=6, hide_code=True)
+def _(mo):
+    mo.md(r"""
+    # SMOTE
+
+    Hypothesis: Since the data is imbalanced (churn = 26.6%), applying SMOTE and other oversampling techniques should improve accuracy.
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _():
+    import imblearn
+    from imblearn.combine import SMOTEENN
+    return (SMOTEENN,)
+
+
+@app.cell(hide_code=True)
+def _(
+    LogisticRegression,
+    SMOTEENN,
+    accuracy_score,
+    label_encoded_data,
+    np,
+    train_test_split,
+):
+    _y = label_encoded_data["churn"]
+    _X = label_encoded_data.drop(columns=["churn"])
+
+    results = []
+
+    for _ in range(10):
+        smote = SMOTEENN()
+        _X_resampled, _y_resampled = smote.fit_resample(_X, _y)
+    
+        _X_train, _X_test, _y_train, _y_test = train_test_split(
+            _X_resampled, _y_resampled, test_size=0.2
+        )
+    
+        smote_le_log_reg = LogisticRegression(max_iter=10_000).fit(_X_train, _y_train)
+        smote_le_preds = smote_le_log_reg.predict(_X_test)
+        smote_le_accuracy = accuracy_score(_y_test, smote_le_preds)
+        results.append(smote_le_accuracy)
+
+    print(f"Highest accuracy: {max(results)} \nAverage accuracy: {np.mean(results)}")
+    return
+
+
+@app.cell(column=7)
 def _():
     return
 
